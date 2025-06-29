5d5fdbdb32fcb6d98ba25c8d0a25968699ba2d23
Ali Maghsoudi <alimaghsoudi75@yahoo.com>
2025-06-29 19:57:04 +0330

    update to wagmi v1.6.4 and viem

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,24 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
-import { WagmiConfig, createConfig, configureChains } from 'wagmi';
+import { WagmiConfig, createClient, configureChains } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { polygonMumbai } from 'wagmi/chains';
-import { createPublicClient, http } from 'viem';
 
-const { chains, publicClient } = configureChains(
+const { provider, webSocketProvider } = configureChains(
   [polygonMumbai],
   [publicProvider()]
 );
 
-const config = createConfig({
+const client = createClient({
   autoConnect: true,
-  publicClient,
+  provider,
+  webSocketProvider,
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <WagmiConfig config={config}>
+    <WagmiConfig client={client}>
       <App />
     </WagmiConfig>
   </React.StrictMode>
